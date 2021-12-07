613b4fc8ed06b73f915eb9f8891e43ce10933cf3
patrickletourneux <patrick.letourneux20@gmail.com>
2021-12-07 20:21:34 +0100

    add page all

--- app/router.js
+++ app/router.js
@@ -3,10 +3,19 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/",(req,res)=>{
-    // const recettes = require("./data/recettes");
     res.render("index");
 });
-router.get("/:name",(req,res)=>{
+
+router.get("/all",(req,res)=>{
+    const coordinates = require("./data/coordinates");
+    console.log('all')
+    res.render("all",{
+        coordinates:coordinates
+    });
+});
+
+
+router.get("/coordinate/:name",(req,res)=>{
     const coordinates = require("./data/coordinates");
     const coordinate = coordinates.find(coordinate => coordinate.name.toLowerCase() == req.params.name.toLowerCase());
     console.log(coordinate);
