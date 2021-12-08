5354c7a9a9b0f5d54f3e41d74f31da080627dd89
patrick letourneux <patrick.letourneux20@gmail.com>
2021-12-08 15:44:17 +0100

    all.ejs is dynamic

--- app/router.js
+++ app/router.js
@@ -3,7 +3,10 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/",(req,res)=>{
-    res.render("index");
+    const coordinates = require("./data/coordinates");
+    res.render("index",{
+        coordinates:coordinates
+    });
 });
 
 router.get("/all",(req,res)=>{
